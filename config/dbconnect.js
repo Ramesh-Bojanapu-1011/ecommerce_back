80f7b93c3ed4f0321b1654648658905662ca05f4
Ramesh Bojanapu <84425972+lovelyram0143@users.noreply.github.com>
2024-05-02 10:11:34 +0530

    minar change

---
+++ config/dbconnect.js
@@ -0,0 +1,27 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Lovelyram:a3aJSTA8vgq1g6yR@cluster0.83s8v4s.mongodb.net/?retryWrites=true&w=majority&appName=cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+const db_connect=run().catch(console.dir);
+
+module.exports=db_connect;
--- index.js
+++ index.js
@@ -1,42 +1,16 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://Lovelyram:o3BrHjWq8ZEAQo7D@cluster0.83s8v4s.mongodb.net/?retryWrites=true&w=majority&appName=cluster0";
-const express= require("express")
-const app=express()
-const doenv=require("dotenv").config()
-const port=5000;
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+const db_connect = require("./config/dbconnect");
+const express = require("express")
+const app = express()
+const PORT =process.env.PORT||4000;
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
-
-
-app.get("/",(_req,res)=>{
-  
-  res.json({
-    name:"Lovely",
-    message:"Hello World"
-  })
+app.get("/",(req,res)=>{
+    res.json("hello world")
 })
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+db_connect
