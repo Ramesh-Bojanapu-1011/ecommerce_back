f544877387d9474c6863debeb3ac3a374bd9bab6
Ramesh Bojanapu <84425972+lovelyram0143@users.noreply.github.com>
2024-05-02 23:53:11 +0530

    change

--- contoller/usrecontrol.js
+++ contoller/usrecontrol.js
@@ -2,9 +2,12 @@ const usermodel = require("../models/usermodel");
 
 const createUser=async(req,res)=>{
     const email=req.body.email
+    const mobile=req.body.mobile
     const user=await usermodel.findOne({email:email})
-    if(user){
-        res.send({"msg":"user already exists"})
+    const user_mobile=await usermodel.findOne({mobile:mobile})
+
+    if(user||user_mobile){
+        res.send({"msg":`user already exists ${user_mobile}`})
     }
     else{
         usermodel.create(req.body)
