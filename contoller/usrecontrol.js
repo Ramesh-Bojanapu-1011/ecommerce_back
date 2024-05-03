669813b50b8669d29bef4c601d2ad6476973dbbe
Ramesh Bojanapu <84425972+lovelyram0143@users.noreply.github.com>
2024-05-03 09:16:00 +0530

    change erros

--- contoller/usrecontrol.js
+++ contoller/usrecontrol.js
@@ -1,20 +1,30 @@
 const usermodel = require("../models/usermodel");
+const asyncHandler = require("express-async-handler");
 
-const createUser=async(req,res)=>{
-    const email=req.body.email
-    const mobile=req.body.mobile
-    const user=await usermodel.findOne({email:email})
-    const user_mobile=await usermodel.findOne({mobile:mobile})
+const createUser = asyncHandler(async (req, res) => {
+  const email = req.body.email;
+  const mobile = req.body.mobile;
+  const user = await usermodel.findOne({ email: email });
+  const user_mobile = await usermodel.findOne({ mobile: mobile });
+  if (user && user_mobile) {
+    res.send({
+      msg: `user already exists ${user.email} and ${user_mobile.mobile}`,
+      
+    });
+  }
 
-    if(user||user_mobile){
-        res.send({"msg":`user already exists ${user_mobile}`})
+  if (user_mobile || user) {
+    if (user_mobile) {
+      res.send({ msg: `user already exists ${user_mobile.mobile}` });
+      
+    } else {
+      res.send({ msg: `user already exists ${user.email}` });
     }
-    else{
-        usermodel.create(req.body)
-        
-        res.json({msg:"user created successfully"})
-    }
-    
-}
+  } else {
+    usermodel.create(req.body);
 
-module.exports={createUser};
+    res.json({ msg: "user created successfully" });
+  }
+});
+
+module.exports = { createUser };
