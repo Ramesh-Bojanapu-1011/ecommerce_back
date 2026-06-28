9adb0ff75e7650925aad93cc8ea8867c77be9726
Ramesh Bojanapu <84425972+Ramesh-Bojanapu-1011@users.noreply.github.com>
2026-06-28 20:31:46 +0530

    Update emailcontroler.js

--- contoller/emailcontroler.js
+++ contoller/emailcontroler.js
@@ -3,6 +3,9 @@ const asyncHandler = require('express-async-handler');
 
 const sendEmail = asyncHandler(async (data) => {
   const transporter = nodemailer.createTransport({
+host: "smtp.gmail.com",
+port: 587,
+  secure: false, // true for port 465, false for 587
     service: 'gmail',
     auth: {
       user: process.env.EMAIL_USERNAME,
